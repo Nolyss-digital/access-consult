776b25214f9a53c856669e41e5c819b5d16c6e4f
MedMejri1 <mejrief@gmail.com>
2026-01-15 10:49:38 +0100

    done

--- src/components/ClientThree.jsx
+++ src/components/ClientThree.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import Slider from "react-slick";
 const ClientThree = () => {
     let settings = {
--- src/components/HeroTwo.jsx
+++ src/components/HeroTwo.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import TrackVisibility from "react-on-screen";
-import CountUp from "react-countup";
+// import TrackVisibility from "react-on-screen";
+// import CountUp from "react-countup";
 import { Link } from "react-router-dom";
 import ModalVideo from 'react-modal-video';
 const HeroTwo = () => {
