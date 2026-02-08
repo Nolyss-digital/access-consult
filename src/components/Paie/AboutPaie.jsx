b47435263f43098a14101690820866d422e0fbf5
MedMejri1 <mejrief@gmail.com>
2026-02-08 22:01:48 +0100

    done

--- src/components/Paie/AboutPaie.jsx
+++ src/components/Paie/AboutPaie.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import TrackVisibility from "react-on-screen";
-import CountUp from "react-countup";
 import { Link } from "react-router-dom";
 
 const AboutPaie = () => {
